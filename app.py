b7003e4a85788e2b9d25adabba4fdd04e82f5c4b
X1a0NaN <167204080+X1a0NaN@users.noreply.github.com>
2025-02-14 06:43:53 +0000

    feat: include gemini into app

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 from flask import Flask, request, render_template
+import google.generativeai as genai
+import os
+
+#############################
+#                           #
+#   Initiate model config   #
+#                           #
+#############################
+
+api = os.getenv("api")
+
+genai.configure(api_key= api)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+#######################
+#                     #
+#   Initiate Web App  #
+#                     #
+#######################
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET","POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/makersuite", methods = ["GET","POST"])
+def makersuite():
+    return render_template("makersuite.html")
+
+@app.route("/gemini", methods = ["GET","POST"])
+def gemini():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return render_template("gemini.html", r = r.candidates[0].content.parts[0].text)
 
 if __name__ == "__main__":
     app.run()
